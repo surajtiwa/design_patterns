729440d23548daa90318e0ead5af6d4bc9dfc97f
suratjiwa <surajtiwary8@gmail.com>
2024-05-26 12:26:04 +0530

    Builder design pattern

---
+++ src/main/java/designPatterns/creational/builder/example1/Client.java
@@ -0,0 +1,39 @@
+package designPatterns.creational.builder;
+
+import java.time.LocalDate;
+//Builder Pattern is used to create Complex Object in a simplified way
+//This is our client which also works as "director"
+public class Client {
+
+    public static void main(String[] args) {
+		User user=createUser();
+		UserDTOBuilder builder=new UserWebDTOBuilder();
+		UserDTO dto=directBuild(builder,user);
+		System.out.println(dto);
+
+    }
+
+    //Director Using the builder to create the dto
+    private static UserDTO directBuild(UserDTOBuilder builder, User user) {
+        return builder.withFirstName(user.getFirstName()).withLastName(user.getLastName()).withAddress(user.getAddress()).
+                withBirthday(user.getBirthday()).build();
+    }
+
+    /**
+     * Returns a sample user.
+     */
+    public static User createUser() {
+        User user = new User();
+        user.setBirthday(LocalDate.of(1960, 5, 6));
+        user.setFirstName("Ron");
+        user.setLastName("Swanson");
+        Address address = new Address();
+        address.setHouseNumber("100");
+        address.setStreet("State Street");
+        address.setCity("Pawnee");
+        address.setState("Indiana");
+        address.setZipcode("47998");
+        user.setAddress(address);
+        return user;
+    }
+}
---
+++ src/main/java/designPatterns/creational/builder/example1/UserDTO.java
@@ -0,0 +1,11 @@
+package designPatterns.creational.builder;
+
+//Interface implemented by "products"
+public interface UserDTO {
+	
+	String getName();
+
+	String getAddress();
+
+	String getAge();
+}
